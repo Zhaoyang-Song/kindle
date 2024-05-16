bda34cb70e513b4d7998cd96a58f1bdb9cdce9a6
Zhaoyang Song <2634367971@qq.com>
2024-05-17 00:30:27 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -12,4 +12,8 @@ echo search_main/search.py run complete
 echo Open the generated local web page
 start "" index.html
 
+echo Deleting unnecessary files and folders...
+for /d %%d in (*) do if /I not "%%d"=="books" if /I not "%%d"=="search_main" if /I not "%%d"=="style" rd /s /q "%%d"
+for %%f in (*) do if /I not "%%f"=="clippings.json" if /I not "%%f"=="date.json" if /I not "%%f"=="index.html" del /f /q "%%f"
+
 pause
